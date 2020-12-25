3bdc83a9b9d5f5da775320c3be77e08e06a90655
pao.s w <david840422@gmail.com>
2020-12-25 15:08:51 +0800

    feat: scala basic syntax test

---
+++ src/per/pao/example/syntax/basic/define/variable/VariableTest.scala
@@ -0,0 +1,26 @@
+package per.pao.example.syntax.basic.define.variable
+
+/**
+ * val similar to final in java
+ *
+ * var 可以改變及重新指派新的 reference
+ */
+object VariableTest {
+
+  def valTest(): Unit = {
+    val num = 5;
+    //    num = 10;
+  }
+
+  def varTest(): Unit = {
+    var num = 5;
+    num = 10;
+    println(num)
+  }
+
+  def main(args: Array[String]): Unit = {
+    //    valTest();
+    varTest();
+  }
+
+}
