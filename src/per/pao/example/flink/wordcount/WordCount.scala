8d591dceaed34ec91082fd6eee025f975105b92f
penguinAO <david840422@gmail.com>
2021-01-12 01:23:41 +0800

    word count with batch processing

---
+++ src/per/pao/example/flink/wordcount/WordCount.scala
@@ -0,0 +1,28 @@
+package per.pao.example.flink.wordcount
+
+import org.apache.flink.api.scala._
+
+/**
+ * batch processing word count
+ */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+
+    // 創建執行環境
+    val env = ExecutionEnvironment.getExecutionEnvironment
+
+    // 從文件中讀取數據
+    val path = System.getProperty("user.dir") + "/resources/hello.text"
+    val inputDataSet = env.readTextFile(path)
+
+    // word count process
+    // 切分數據得到 word 再做分組聚合
+    val wordCountDataSet = inputDataSet.flatMap(_.split(" "))
+      .map((_, 1))
+      .groupBy(0)
+      .sum(1)
+
+    wordCountDataSet.print()
+  }
+
+}
